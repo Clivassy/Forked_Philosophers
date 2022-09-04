272607b5a6f3e0480c46e071a5bd3f9fca052e04
Julia BATORO <jbatoro@e1r1p1.clusters.42paris.fr>
2022-09-04 16:09:40 +0200

    ok

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -10,11 +10,6 @@
 
 # define NORMAL  "\x1B[0m"
 # define RED  "\x1B[31m"
-# define GREEN  "\x1B[32m"
-# define YELLOW  "\x1B[33m"
-# define BLUE  "\x1B[34m"
-# define MAGENTA  "\x1B[35m"
-# define CYAN  "\x1B[36m"
 
 # define EATING "is eating"
 # define ENOUGH_EAT "eat enough"
